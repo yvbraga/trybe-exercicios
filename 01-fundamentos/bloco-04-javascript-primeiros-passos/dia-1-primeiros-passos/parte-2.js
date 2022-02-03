b15a8363988e9524bb319ad7846e3073b5a75c5b
Yuri Braga <yvbraga@protonmail.com>
2022-02-03 11:57:59 -0300

    exercicios javascript

---
+++ 01-fundamentos/bloco-04-javascript-primeiros-passos/dia-1-primeiros-passos/parte-2.js
@@ -0,0 +1,18 @@
+let resultado = 100;
+
+switch (resultado) {
+    case resultado >= 80:
+    console.log("aprovada");
+    break;
+
+    case resultado >= 60:
+    console.log("lista");
+    break;
+
+    case resultado < 60:
+    console.log("reprovada");
+    break;
+
+    default:
+    console.log("não se aplica")
+}
