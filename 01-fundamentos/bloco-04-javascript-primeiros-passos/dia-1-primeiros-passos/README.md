42fa51e8eee83953a139ca9dae18e45f71fd8a09
Yuri Braga <yvbraga@protonmail.com>
2022-02-04 10:37:19 -0300

    exercicios js

--- 01-fundamentos/bloco-04-javascript-primeiros-passos/dia-1-primeiros-passos/README.md
+++ 01-fundamentos/bloco-04-javascript-primeiros-passos/dia-1-primeiros-passos/README.md
@@ -31,3 +31,7 @@
 2. Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
 
 3. Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+
+4. Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário. 
+
+5. Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne `true` se os ângulos representarem os ângulos de um triângulo e `false` , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
--- 01-fundamentos/bloco-04-javascript-primeiros-passos/dia-1-primeiros-passos/parte-3.js
+++ 01-fundamentos/bloco-04-javascript-primeiros-passos/dia-1-primeiros-passos/parte-3.js
@@ -1,34 +1,62 @@
-let a = Math.floor(Math.random() * 100) + 1
-let b = Math.floor(Math.random() * 100) + 1
+// let a = Math.floor(Math.random() * 100) + 1;
+// let b = Math.floor(Math.random() * 100) + 1;
 
-console.log(`primeiro número: ${a}`)
-console.log(`segundo número: ${b}`)
+// console.log(`primeiro número: ${a}`);
+// console.log(`segundo número: ${b}`);
 
-let soma = a + b
-console.log(`${a} + ${b} é igual a : ${soma}`)
+// let soma = a + b;
+// console.log(`${a} + ${b} é igual a : ${soma}`);
 
-let sub = a - b
-console.log(`${a} - ${b} é igual a: ${sub}`)
+// let sub = a - b;
+// console.log(`${a} - ${b} é igual a: ${sub}`);
 
-let mult = a * b
-console.log(`${a} * ${b} é igual a : ${mult}`)
+// let mult = a * b;
+// console.log(`${a} * ${b} é igual a : ${mult}`);
 
-let div = a / b
-console.log(`${a} / ${b} é igual a : ${div.toFixed(2)}`)
+// let div = a / b;
+// console.log(`${a} / ${b} é igual a : ${div.toFixed(2)}`);
 
-let mod = a % b
-console.log(`o resto da divisão entre ${a} e ${b} é: ${mod}`)
+// let mod = a % b;
+// console.log(`o resto da divisão entre ${a} e ${b} é: ${mod}`);
 
-if (a > b) {
-    console.log(`${a} é o maior número`)
+// if (a > b) {
+//     console.log(`${a} é o maior número`);
+// }
+// else if (b > b) {
+//     console.log(`${b} é o maior número`);
+// }
+// else {
+//     console.log(`os números são iguais`);
+// }
+
+// if (a > 0) {
+//     console.log(`o número ${a} é um número positivo`);
+// }
+// else if (a < 0) {
+//     console.log(`o número ${a} é um número negativo`);
+// }
+// else {
+//     console.log(`o número ${a} é um número neutro`);
+// }
+
+// if (b > 0) {
+//     console.log(`o número ${b} é um número positivo`);
+// }
+// else if (b < 0) {
+//     console.log(`o número ${b} é um número negativo`);
+// }
+// else {
+//     console.log(`o número ${b} é um número neutro`);
+// }
+
+let lado1 = Math.floor(Math.random() * 100) + 1;
+let lado2 = Math.floor(Math.random() * 100) + 1;
+let lado3 = Math.floor(Math.random() * 100) + 1;
+
+console.log(`lado 1: ${lado1}, lado 2: ${lado2}, lado 3: ${lado3}`);
+
+if (lado1 + lado2 + lado3 === 180) {
+    console.log(`é um triângulo`);
+} else {
+    console.log(`não é um triângulo`);
 }
-else if (b > b) {
-    console.log(`${b} é o maior número`)
-}
-else {
-    console.log(`os números são iguais`)
-}
-
-let num1 = Math.floor(Math.random() * 100) + 1;
-let num2 = Math.floor(Math.random() * 100) + 1;
-let num3 = Math.floor(Math.random() * 100) + 1;
