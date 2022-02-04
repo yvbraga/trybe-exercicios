81923245bee5d57472963d5dc55ec4cf92165a3c
Yuri Braga <yvbraga@protonmail.com>
2022-02-04 10:53:28 -0300

    exercicios js for

---
+++ 01-fundamentos/bloco-04-javascript-primeiros-passos/dia-2-array-e-loop-for/script.js
@@ -0,0 +1,20 @@
+// let pizzas = ["4 Queijos", "Frango com catupiry", "Marguerita", "Palmito", "Chocolate"];
+
+// pizzas.push("Peito de Peru");
+// pizzas.push("Atum");
+
+// for (let i = 0; i < pizzas.length; i += 1) {
+//     console.log(pizzas.sort()[i])
+// }
+
+// let num = 1;
+
+// for (let i = 1; i <= 9; i += 1) {
+//     console.log(num * i);
+// }
+
+let nomes = ["Joana", "Maria", "Lucas"];
+
+for (let i = 0; i < nomes.length; i += 1) {
+    console.log(`Bem vindo(a) ${nomes[i]} !`);
+}
